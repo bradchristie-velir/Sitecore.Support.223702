e3cc09c8449e3beb7d2a2fea901cd141097da57d
DK\mao <mao@sitecore.net>
2018-06-06 11:48:20 +0300

    Add default implementation of the CleanupAuthenticationTicketsAgent.

--- src/Sitecore.Support.223702/Tasks/CleanupAuthenticationTicketsAgent.cs
+++ src/Sitecore.Support.223702/Tasks/CleanupAuthenticationTicketsAgent.cs
@@ -1,11 +1,38 @@
-﻿using System;
+﻿using Sitecore.Abstractions;
+using Sitecore.Diagnostics;
+using Sitecore.Web.Authentication;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 
 namespace Sitecore.Support.Tasks
 {
   public class CleanupAuthenticationTicketsAgent
   {
+    public void Run()
+    {
+      List<string> ticketIDs = TicketManager.GetTicketIDs();
+      int countRemovedTickets = 0;
+
+      Log.Info(string.Format("CleanupAuthenticationTicketsAgent: Total number of authentication tickets to process: {0}", ticketIDs.Count), this);
+
+      foreach (string ticketID in ticketIDs)
+      {
+        Ticket ticket = TicketManager.GetTicket(ticketID, true);
+
+        if (ticket != null)
+        {
+          if (TicketManager.IsTicketExpired(ticket, false))
+          {
+            TicketManager.RemoveTicket(ticket.Id);
+            countRemovedTickets++;
+          }
+        }
+      }
+
+      Log.Info(string.Format("CleanupAuthenticationTicketsAgent: Number of expired authentication tickets that have been removed: {0}", countRemovedTickets), this);
+    }
   }
 }
